a50d12b6c5efd452647f3272ec122da45d616b6f
MSousa <msousacode@gmail.com>
2025-07-20 18:52:00 -0300

    commit

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header>
-      <div class="row justify-center q-pa-md shadow-4">
+    <q-header elevated class="bg-white text-primary">
+      <div class="row justify-center q-pa-md">
         <q-btn flat no-caps label="Sobre Nós" size="17px"></q-btn>
         <q-btn flat no-caps label="Planos" size="17px"></q-btn>
         <q-btn flat no-caps label="Perguntas Frequentes" size="17px"></q-btn>
@@ -11,6 +11,12 @@
     <q-page-container>
       <IndexPage />
     </q-page-container>
+
+    <q-footer class="text-center bg-white text-primary">
+      <div class="row justify-center q-pa-md">
+        <span class="text-caption"> © 2024 Vamos ver. Todos os direitos reservados. </span>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
