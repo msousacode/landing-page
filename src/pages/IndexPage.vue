8e8ae8f258908bf848c29ec36f9cd4e68da9808a
MSousa <msousacode@gmail.com>
2025-07-20 17:56:42 -0300

    primeiro committ

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,142 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <div class="row justify-center items-center">
+    <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md q-mt-xl">
+      <span class="text-h5 text-bold">
+        Agora a Sua <span class="text-secondary text-h5 text-bold">Corretora de Seguros</span> pode
+        contar com o <span class="text-secondary text-h5 text-bold">Poder</span> da
+        <span class="text-secondary text-h5 text-bold">Inteligência Artificial</span> para
+        automatizar
+        <span class="text-secondary text-h5 text-bold">Processos, Reduzir Custos</span> e
+        <span class="text-secondary text-h5 text-bold">Aumentar a Eficiência Operacional</span>.
+        <br />
+        <br />
+        Agende uma Conversa para <span class="text-secondary text-h5 text-bold">Saber Mais!</span>
+      </span>
+
+      <div class="q-mt-md"></div>
+
+      <q-btn color="secondary" label="Fale conosco no WhatsApp" size="16px" no-caps />
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md">
+      <img
+        src="https://assini.com.br/wp-content/uploads/2024/01/hero-1-1-1024x867.png"
+        alt="Landing Page Image"
+        style="max-width: 500px; width: 100%; height: auto"
+      />
+    </div>
+  </div>
+
+  <div class="q-mt-sm"></div>
+
+  <div class="row justify-center items-center">
+    <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md">
+      <img
+        src="https://assini.com.br/wp-content/uploads/2024/01/section-1.png"
+        alt="Landing Page Image"
+        style="max-width: 500px; width: 100%; height: auto"
+      />
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md q-mt-xl">
+      <span class="text-h5">
+        Agora a Sua <span class="text-secondary text-h5">Corretora de Seguros</span> pode contar com
+        a <span class="text-secondary text-h5">Inteligência Artificial</span> para automatizar
+        <span class="text-secondary text-h5">Processos, Reduzir Custos</span> e
+        <span class="text-secondary text-h5">Aumentar a Eficiência Operacional</span>.
+        <br />
+        <br />
+        Fale conosco no WhatsApp<span class="text-secondary text-h5">Saber Mais!</span>
+      </span>
+
+      <div class="q-mt-md"></div>
+    </div>
+  </div>
+
+  <div class="row justify-center">
+    <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md q-mt-xl">
+      <span class="text-h5 text-bold">
+        ESTÁ COM DÙVIDAS? Perguntas Frequentes Tudo que você precisa saber sobre a Assini, o maior
+        portal de assinatura eletrônica online. Tem mais perguntas?
+
+        <br />
+        <br />
+        Agende uma Conversa para <span class="text-secondary text-h5 text-bold">Saber Mais!</span>
+      </span>
+
+      <div class="q-mt-md"></div>
+
+      <q-btn color="secondary" label="Fale conosco no WhatsApp" size="16px" no-caps />
+    </div>
+
+    <div class="col-xs-12 col-sm-6 col-md-4 q-pa-md q-mt-xl">
+      <q-list bordered class="rounded-borders">
+        <q-expansion-item
+          expand-separator
+          label="O que é a Assini e como ela pode ajudar minha empresa?"
+          class="text-h6"
+        >
+          <q-card>
+            <q-card-section class="text-body1">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+        <q-expansion-item
+          expand-separator
+          label="A assinatura eletrônica da Assini tem validade jurídica?"
+          class="text-h6"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+        <q-expansion-item
+          expand-separator
+          label="Quais tipos de modelos de Assinatura Eletrônica disponíveis na Assini?"
+          class="text-h6"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+
+        <q-expansion-item
+          label="É necessário instalar algum software para criar assinatura eletrônica na Assini?"
+          class="text-h6"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+        <q-expansion-item
+          label="Posso cancelar minha assinatura eletrônica da Assini a qualquer momento?"
+          class="text-h6"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit
+              eos corrupti commodi magni quaerat ex numquam, dolorum officiis modi facere maiores
+              architecto suscipit iste eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
+      </q-list>
+    </div>
+  </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
-</script>
+<script setup lang="ts"></script>
